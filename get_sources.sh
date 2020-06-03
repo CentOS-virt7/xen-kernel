e0c6211a2005d9d101f387858ebcd907d09e9e63
Anthony PERARD <anthony.perard@citrix.com>
2020-06-03 13:06:00 +0100

    some test with get_sources.sh

--- get_sources.sh
+++ get_sources.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+
 source sources.cfg
 echo "Checking Linux $LINUX_VERSION release tarball"
 if [[ ! -e SOURCES/$LINUX_RELEASE_FILE ]] ; then
@@ -11,5 +13,14 @@ fi
 echo "Uncompressing release file and checking signature"
 xz -cd SOURCES/$LINUX_RELEASE_FILE | gpg2 --status-fd 1 --verify SOURCES/$LINUX_RELEASE_FILE_SIG - \
     | grep -q "GOODSIG ${LINUX_KEY}" || exit 1
+
+# Check that we've got the expected Linux version
+shasums=$(curl -sL https://www.kernel.org/pub/linux/kernel/v${LINUX_VERSION%%.*}.x/sha256sums.asc)
+# Check against autosigner@kernel.org keys
+gpg2 --status-fd=1 --verify - <<<"$shasums" | \
+    grep -q -E '^\[GNUPG:\] GOODSIG 632D3A06589DA6B1' || exit 1
+
+grep -E "^[0-9a-f]{64}  $LINUX_RELEASE_FILE$" <<<"$shasums" | \
+    (cd SOURCES; sha256sum -c -) || exit 1
 
 echo "All sources present."
